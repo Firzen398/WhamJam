62822546607ef644f8e6f45a2c62eb8ce68b6b5b
matt <matthias.grumet@greentube.com>
2018-12-14 21:05:03 +0100

    reading datafile

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
-{
+{ 
+    const string LYRICSFILE = "Assets/Data/lyrics.json";
+
+
     [SerializeField]
     private SongManager songManager;
 
 
-    private void StartGame()
+    public Word [] WordsArray;
+
+    private int wordIndex;
+
+    private void Start()
     {
         songManager.GameStart();
+
+        using (var reader = new StreamReader(LYRICSFILE))
+        {
+            WordsArray = (Word[])JsonConvert.DeserializeObject<Word[]>(reader.ReadToEnd());
+        }
+        
     }
+
+    private void Update()
+    {
+        if (wordIndex >= 0 && wordIndex < WordsArray.Length)
+        {
+            Word word = WordsArray[wordIndex];
+
+
+            if (Time.realtimeSinceStartup > word.StartTime)
+            {
+                Debug.Log($"{word.StartTime} : {word.Text}");
+                wordIndex++;
+            }
+
+
+
+        }
+
+    }
+}
+
+[JsonObject]
+public class Word
+{
+    [JsonProperty("starttime")]
+    public float StartTime;
+
+    [JsonProperty("endtime")]
+    public float EndTime;
+
+    [JsonProperty("text")]
+    public string Text;
 }
