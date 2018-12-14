98dd668fea3a2e090ac1418bb7988e6c0c81368f
Firzen398 <tadej.ganza@gmail.com>
2018-12-14 21:34:53 +0100

    Updated BubbleManager and BubbleScript

--- Assets/Scripts/BubbleManager.cs
+++ Assets/Scripts/BubbleManager.cs
@@ -6,12 +6,62 @@ public class BubbleManager : MonoBehaviour
 {
     [SerializeField]
     float width = 10;
+
     [SerializeField]
     float height = 10;
 
+    [SerializeField]
+    int nrOfBubbles = 10;
+
+    [SerializeField]
+    BubbleScript bubblePrefab;
+
+    List<BubbleScript> availableBubbles = new List<BubbleScript>();
+
+    public void Start()
+    {
+        for(int i = 0; i < nrOfBubbles; i++)
+        {
+            BubbleScript bubble = Instantiate<BubbleScript>(bubblePrefab);
+            bubble.transform.SetParent(transform);
+        }
+    }
+
+    /// <summary>
+    /// Returns the bubble that was spawned.
+    /// </summary>
+    /// <param name="word"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    public BubbleScript SpawnNewBubble(string word, float duration)
+    {
+        BubbleScript obj ;
+        int lastAvailableIndex = availableBubbles.Count - 1;
+        if (lastAvailableIndex >= 0)
+        {
+            obj = availableBubbles[lastAvailableIndex];
+            availableBubbles.RemoveAt(lastAvailableIndex);
+            obj.gameObject.SetActive(true);
+
+            Vector3 position = new Vector3(Random.Range(-width / 2, width / 2), 0, Random.Range(-height / 2, height / 2));
+            Vector3 direction = new Vector3(Random.Range(-width / 2, width / 2), 0, Random.Range(-height / 2, height / 2));
+
+            obj.Initialise(position, direction.normalized, word);
+            return obj;
+        }
+
+        // Error.
+        return null;
+    }
+
+    public void PopBubble(BubbleScript obj)
+    {
+        availableBubbles.Add(obj);
+    }
+
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.blue;
         float wHalf = (width * .5f);
         float hHalf = (height * .5f);
 
--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BubbleScript : MonoBehaviour
 {
     [SerializeField]
     private string word;
 
-    [SerializeField]
-    private float holdTime;
-
     private Vector3 direction;
-
     private float speed = 1f;
+
+    private float maxX;
+    private float maxY;
 
     void Update()
     {
-        transform.position += direction * speed;
+        transform.position += direction * speed;        
+    }
+
+    public void Initialise(Vector3 position, Vector3 direction, string word)
+    {
+        transform.position = position;
+        this.direction = direction;
+        this.word = word;
+    }
+    
+    public void BubblePop()
+    {
+
     }
 }
