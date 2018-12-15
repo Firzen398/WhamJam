6497518e3e03a7e8528d8a46cbd7cfc30aab1cab
matt <matthias.grumet@greentube.com>
2018-12-15 15:31:19 +0100

    add animation to background

---
+++ Assets/Scripts/BackgroundAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject background;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = (float) Math.Sin(Time.realtimeSinceStartup);
+        float y = (float) Math.Sin(2*Time.realtimeSinceStartup+1);
+
+         background.transform.position = new Vector3(x,y,0);
+    }
+}
